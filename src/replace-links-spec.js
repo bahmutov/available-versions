9e65d4d9470a35ba63b71f5902c68c4542c4f291
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-08-09 10:56:41 -0400

    removing links

--- src/replace-links-spec.js
+++ src/replace-links-spec.js
@@ -13,32 +13,39 @@ describe('replace links', () => {
   it('replaces SHA links', () => {
     const s = 'foo ([a00ed3e7](https://something/a00ed3e7))';
     const r = replaceLinks(s);
-    la(r === 'foo a00ed3e7', r);
+    la(r === 'foo (a00ed3e7)', r);
   });
 
   it('replaces SHA links surrounded by text', () => {
     const s = 'foo ([a00ed3e7](https://something/a00ed3e7)) bar';
     const r = replaceLinks(s);
-    la(r === 'foo a00ed3e7 bar', r);
+    la(r === 'foo (a00ed3e7) bar', r);
   });
 
   it('replaces in full github url', () => {
     const s = '([c1e87cc9](https://github.com/bahmutov/manpm/commit/c1e87cc9))';
     const r = replaceLinks(s);
-    la(r === 'c1e87cc9', r);
+    la(r === '(c1e87cc9)', r);
   });
 
   it('replaces in longer string', () => {
     const s = 'Finds section by text in the heading ' +
       '([c1e87cc9](https://github.com/bahmutov/manpm/commit/c1e87cc9))';
     const r = replaceLinks(s);
-    la(r === 'Finds section by text in the heading c1e87cc9', r);
+    la(r === 'Finds section by text in the heading (c1e87cc9)', r);
   });
 
   it('replaces in string with markdown', () => {
     const s = '* **search:** Finds section by text in the heading ' +
       '([c1e87cc9](https://github.com/bahmutov/manpm/commit/c1e87cc9))';
     const r = replaceLinks(s);
-    la(r === '* **search:** Finds section by text in the heading c1e87cc9', r);
+    la(r === '* **search:** Finds section by text in the heading (c1e87cc9)', r);
+  });
+
+  it('replaces urls in string with two links', () => {
+    const s = '([a360d56e](https://github.com/bahmutov/manpm/commit/a360d56e)' +
+      ', closes [#11](https://github.com/bahmutov/manpm/issues/11))';
+    const r = replaceLinks(s);
+    la(r === '(a360d56e, closes #11)', r);
   });
 });

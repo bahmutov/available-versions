3e19e3e5dd4d875b56e69e5ef7052f18f2b791a5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-08-17 12:56:21 -0400

    chore(gitlab): refactoring for gitlab fetching

--- src/fetch-release-notes.js
+++ src/fetch-release-notes.js
@@ -4,6 +4,7 @@ var request = require('axios');
 var la = require('lazy-ass');
 var check = require('check-more-types');
 var debug = require('debug')('vers');
+// var gitlab = require('gitlab');
 
 function urlFromGitHub(user, repo) {
   const githubUrl = 'https://api.github.com/repos/';
@@ -11,26 +12,43 @@ function urlFromGitHub(user, repo) {
   return url;
 }
 
-function supportedServer(type) {
+function isGitHub(type) {
   return type === 'github';
 }
 
-function fetchReleaseNotes(available) {
-  debug('available', available);
+function isGitlab(type) {
+  return type === 'gitlab';
+}
 
-  if (!available.repoParsed) {
-    debug('no parsed repo');
+function hasGitLabToken() {
+  return check.unemptyString(process.env.GITLAB_AUTH_TOKEN);
+}
+
+function supportedServer(type) {
+  return isGitHub(type) || (
+    isGitlab(type) && hasGitLabToken()
+  );
+}
+
+function fetchFromGitlab(repo) {
+  la(repo.server === 'gitlab', 'invalid gitlab server', repo);
+}
+
+function fetchFromGitlab(available, repo) {
+  la(isGitlab(repo.server), 'invalid gitlab server', repo);
+  debug('fetching release notes from gitlab server');
+  debug(repo);
+  if (!hasGitLabToken()) {
+    console.error('Missing gitlab access token');
     return available;
   }
-  if (!supportedServer(available.repoParsed.server)) {
-    debug('do not know how to fetch releases from %s',
-      available.repoParsed.server);
-    return available;
-  }
+  return available;
+}
 
-  const url = urlFromGitHub(available.repoParsed.user,
-    available.repoParsed.repo);
-  debug('fetching releases from %s', url);
+function fetchFromGitHub(available, repo) {
+  la(isGitHub(repo.server), 'invalid github server', repo);
+  const url = urlFromGitHub(repo.user, repo.repo);
+  debug('fetching releases from github %s', url);
 
   return request.get(url)
     .then(function (r) {
@@ -49,6 +67,30 @@ function fetchReleaseNotes(available) {
       debug(err);
       return available;
     });
+}
+
+function fetchReleaseNotes(available) {
+  debug('available', available);
+
+  if (!available.repoParsed) {
+    debug('no parsed repo');
+    return available;
+  }
+
+  var type = available.repoParsed.server;
+  if (!supportedServer(type)) {
+    debug('do not know how to fetch releases from %s', type);
+    return available;
+  }
+
+  if (isGitHub(type)) {
+    return fetchFromGitHub(available, available.repoParsed);
+  }
+  if (isGitlab(type)) {
+    return fetchFromGitlab(available, available.repoParsed);
+  }
+
+  return available;
 }
 
 module.exports = fetchReleaseNotes;

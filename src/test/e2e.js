2b151a0ed65a9abade501fa70630308c40e5e85e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 10:47:09 -0400

    test with scoped name

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -27,6 +27,18 @@ gt.async('deps-ok with version', function () {
   });
 });
 
+gt.async('@bahmutov/csv with scope', function () {
+  available({
+    name: '@bahmutov/csv'
+  }).then(function (info) {
+    gt.equal(info.name, '@bahmutov/csv');
+  }).catch(function (err) {
+    gt.ok(false, 'cannot fetch info, error', err);
+  }).then(function () {
+    gt.start();
+  });
+});
+
 gt.async('deps-ok as string', function () {
   available('deps-ok')
   .then(function (info) {

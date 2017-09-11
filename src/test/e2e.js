e1bea1c152f38037e760e771d49e7ed2a9e3058b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-09-11 10:44:29 -0400

    test with version

--- src/test/e2e.js
+++ src/test/e2e.js
@@ -14,6 +14,19 @@ gt.async('deps-ok', function () {
   });
 });
 
+gt.async('deps-ok with version', function () {
+  available({
+    name: 'deps-ok',
+    version: '1.0.0'
+  }).then(function (info) {
+    gt.equal(info.name, 'deps-ok');
+  }).catch(function (err) {
+    gt.ok(false, 'cannot fetch info, error', err);
+  }).then(function () {
+    gt.start();
+  });
+});
+
 gt.async('deps-ok as string', function () {
   available('deps-ok')
   .then(function (info) {
